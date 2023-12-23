0fa254a0ed52d11e065bbbfd19965fdf0da73f3f
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2023-12-23 12:56:48 +0530

    add

---
+++ Introduction_to_SQL/1.sql
@@ -0,0 +1,27 @@
+
+-- Welcome to the 
+-- 1
+-- �
+-- �
+-- 1 
+-- st
+--   module on Learn SQL syntax. Some important points
+
+-- At the end of this learning path, you will be able to understand and interpret queries written in SQL
+-- Please feel free to give us your feedback for each problem / solution / module.
+-- Help us get better.
+-- Structured Query Language (SQL) is a programming language specifically created to access, organise and manipulate data within relational databases. In a relational database, information is arranged into one or more tables, each containing columns and rows of interconnected data entries that relate to each other in some way.
+
+-- Let's begin!!! Let us output all the data entered in the table "employee"
+
+
+-- Click on Submit to check the output.
+-- Click on Next to proceed
+
+
+
+
+/*Click on Submit to check the output.
+Click on Next to proceed */
+
+SELECT * from employee;
