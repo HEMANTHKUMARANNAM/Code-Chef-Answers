8f7cf96c8e317c77be1276a5342b417f2b447be3
HEMANTH KUMAR <hemanthkumar312004@gmail.com>
2024-01-02 17:52:56 +0530

    ADD

--- Module 5 Intermediate SQL/Multiple tables - I/1.sql
+++ Module 5 Intermediate SQL/Multiple tables - I/1.sql
@@ -1,3 +1,14 @@
+-- Welcome to the Intermediate level course on SQL.
 
+-- Below are some of the basic and beginner level queries that we covered in Learn SQL.
 
-  WILL BE UPLOADED SOON
+-- Table / db manipulation
+-- CREATE, ALTER, INSERT, DELETE allow us to create a table or make changes to an existing table
+-- Queries
+-- SELECT allows us to view entries in a table
+-- WHERE, BETWEEN, LIKE, AND, OR can be added along with SELECT to check which entires meet certain conditions
+-- Aggregate functions
+-- SQL also allows us to use aggregate functions such as COUNT, MAX / MIN, SUM, AVG to view aggregate information of the table
+-- GROUP BY statement in SQL are used to combine rows with identical values into summary rows.
+-- GROUP BY is frequently used with the syntax HAVING to apply filters at a group level
+-- Lets proceed and learn some new Intermediate level concepts!
